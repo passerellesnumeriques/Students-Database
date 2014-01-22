8a6a47d7ab9bf6cf2afe44e757fdfcdd34cb2dbb
Nicolas Debard <nicolas.debard@passerellesnumeriques.org>
2014-01-22 10:04:03 +0800

    update 01/22
    Signed-off-by: Nicolas Debard <nicolas.debard@passerellesnumeriques.org>

--- www/component/selection/page/exam_subject.php
+++ www/component/selection/page/exam_subject.php
@@ -5,6 +5,8 @@ class page_exam_subject extends selection_page {
 	public function get_required_rights() { return array(); }
 	public function execute_selection_page(&$page){
 		$container_id = $page->generateID();
+		$page->add_javascript("/static/widgets/page_header.js");
+		$page->onload("var header = new page_header('exam_subject_header',true); header.setTitle('Exam Subject');");
 		$id = null;
 		if(!isset($_GET["id"]))
 			$id = -1;
@@ -20,7 +22,15 @@ class page_exam_subject extends selection_page {
 		else
 			$read_only = false;
 	?>
-		<div id = '<?php echo $container_id; ?>'>
+		<div id = 'exam_subject_header'>
+			<!--<div id = "rename_exam_subject"></div>
+			<div id = "edit_exam_subject"></div>
+			<div id = "unedit_exam_subject"></div>
+			<div id = "unedit_exam_subject"></div>
+			<div id = "save_exam_subject"></div>
+			<div id = "export_exam_subject"></div>-->
+		</div>
+		<div id = '<?php echo $container_id; ?>'></div>
 	
 	<?php
 		exam_subject($page,$container_id,$id,$campaign_id,$read_only);
