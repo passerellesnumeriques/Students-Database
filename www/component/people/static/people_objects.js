9785fb8fcd094498347b0de655c8637f081c8ac5
Nicolas Debard <nicolas.debard@passerellesnumeriques.org>
2014-01-29 10:20:57 +0800

    people_object.js IS_objects.js

---
+++ www/component/people/static/people_objects.js
@@ -0,0 +1,21 @@
+/**
+ * People
+ * @param {integer} id
+ * @param {string} first_name
+ * @param {string} last_name
+ * @param {string} middle_name
+ * @param {string} khmer_first_name
+ * @param {string} khmer_last_name
+ * @param {string} sex M or F
+ * @param {date} birth
+ */
+function People(id, first_name, last_name, middle_name, khmer_first_name, khmer_last_name, sex, birth){
+	this.id = id;
+	this.first_name = first_name;
+	this.last_name = last_name;
+	this.middle_name = middle_name;
+	this.khmer_first_name = khmer_first_name;
+	this.khmer_last_name = khmer_last_name;
+	this.sex = sex;
+	this.date = date;
+}
