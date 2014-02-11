ec470c190d6002ef9e02bb6b7eaa10fce6be143e
Nicolas Debard <nicolas.debard@passerellesnumeriques.org>
2014-02-11 11:30:33 +0800

    Correction bugs merge IS + update getCountriesList method from component geography
    Signed-off-by: Nicolas Debard <nicolas.debard@passerellesnumeriques.org>

--- www/component/geography/service/get_countries_list.php
+++ www/component/geography/service/get_countries_list.php
@@ -1,47 +1,22 @@
 <?php
 class service_get_countries_list extends Service{
 	public function get_required_rights(){return array();}
-	public function input_documentation(){}//TODO
-	public function output_documentation(){}//TODO
-	public function documentation(){}//TODO
+	public function input_documentation(){echo "No";}
+	public function output_documentation(){
+		?>
+		Returns an array containing javascript objects:
+		<ul>
+			<li><code>country_id</code> the id of the country in the database</li>
+			<li><code>country_code</code> the international country code</li>
+			<li><code>country_name</code> the name of the country</li>
+		</ul>
+		<?php
+	}
+	public function documentation(){
+		echo "Return an array of all the countries set into the database.<br/>First country of the list is the one of the user domain<br/>Next ones are first the PN countries, then all the others";
+	}
 	public function execute(&$component,$input){
-		$q = SQLQuery::create()->select("Country")
-			->field("Country","id","country_id")
-			->field("Country","code","country_code")
-			->field("Country","name","country_name")
-			->orderBy("Country", "name", true)
-			;
-		$countries = $q->execute();
-		
-		// put the countries of PN domains first
-		$PN_countries = array();
-		foreach (PNApplication::$instance->get_domains() as $domain=>$descr) {
-			$code = $descr["country_code"];
-			$code = strtolower($code);
-			if (!in_array($code, $PN_countries)) array_push($PN_countries, $code);
-		}
-		for ($i = 0; $i < count($countries); $i++) {
-			$code = strtolower($countries[$i]["country_code"]); 
-			if (in_array($code, $PN_countries)) {
-				// found
-				$country = $countries[$i];
-				array_splice($countries, $i, 1); // remove it
-				array_splice($countries, 0, 0, array($country)); // insert it
-			}
-		}
-		
-		// put the domain country first
-		$local_code = PNApplication::$instance->get_domain_descriptor();
-		$local_code = strtolower($local_code["country_code"]);
-		for ($i = 0; $i < count($countries); $i++) {
-			if (strtolower($countries[$i]["country_code"]) == $local_code) {
-				// found
-				$country = $countries[$i];
-				array_splice($countries, $i, 1); // remove it
-				array_splice($countries, 0, 0, array($country)); // insert it
-				break;
-			}
-		}
+		$countries = PNApplication::$instance->geography->getCountriesList();
 		
 		echo "[";
 		$first = true;
