169507b6806ede42fe0263e85d2b45ba842e011a
Guillaume Le Cousin <guillaume.lecousin@passerellesnumeriques.org>
2014-07-02 17:10:13 +0800

    improve people picture: only reference to storage_id+reference, and when changing, automatically updated

--- www/component/people/service/picture.php
+++ www/component/people/service/picture.php
@@ -3,20 +3,26 @@ class service_picture extends Service {
 	public function getRequiredRights() { return array(); }
 	public function documentation() { echo "Retrieve the profile picture of a people"; }
 	public function inputDocumentation() { echo "<code>people</code>: id of the people to get the picture"; }
-	public function outputDocumentation() { echo "The picture"; }
-	public function getOutputFormat($input) { return "image/jpeg"; }
+	public function outputDocumentation() { echo "storage_id and revision, or sex (if no picture)"; }
 	public function execute(&$component, $input) {
-		$people_id = $_GET["people"];
+		$people_id = $input["people"];
 		if (!$component->canRead($people_id)) {
-			header("Location: ".theme::$icons_16["error"]);
+			PNApplication::error("Access denied: you cannot access to the data of this person");
 			return;
 		}
-		$people = SQLQuery::create()->select("People")->whereValue("People", "id", $people_id)->field("picture")->field("sex")->executeSingleRow();
+		$q = SQLQuery::create()
+			->select("People")
+			->whereValue("People", "id", $people_id)
+			->field("picture")
+			->field("sex")
+			;
+		PNApplication::$instance->storage->joinRevision($q, "People", "picture", "revision");
+		$people = $q->executeSingleRow();
 		if ($people["picture"] <> null) {
-			header("Location: /dynamic/storage/service/get?id=".$people["picture"]);
-			return;
+			echo "{storage_id:".$people["picture"].",revision:".$people["revision"]."}";
+		} else {
+			echo "{sex:".json_encode($people["sex"])."}";
 		}
-		readfile(dirname(__FILE__)."/../static/default_".($people["sex"] == "F" ? "female" : "male").".jpg");
 	}
 }
 ?>
