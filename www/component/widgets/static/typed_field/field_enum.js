37a8becb905c35697fb5ff1a7ff96b7c82a78c40
Guillaume Le Cousin <guillaume.lecousin@passerellesnumeriques.org>
2014-08-11 15:49:34 +0800

    fix bug on JoiningTableDataDisplay

--- www/component/widgets/static/typed_field/field_enum.js
+++ www/component/widgets/static/typed_field/field_enum.js
@@ -108,7 +108,7 @@ field_enum.prototype._create = function(data) {
 			return text;
 		};
 		this.element.appendChild(this.text = document.createTextNode(this.get_text_from_data(data)));
-		this.element.style.height = "100%";
+		//this.element.style.height = "100%";
 		this._setData = function(data) {
 			this.text.nodeValue = this.get_text_from_data(data);
 		};
