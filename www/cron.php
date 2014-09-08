21e3cfc853fc0cda72d6c5c6c1c5ce4c093445e9
Guillaume Le Cousin <guillaume.lecousin@passerellesnumeriques.org>
2014-09-08 13:06:58 +0800

    fix cron

--- www/cron.php
+++ www/cron.php
@@ -1,8 +1,13 @@
 <?php
+set_include_path(dirname(__FILE__));
+date_default_timezone_set("GMT");
+global $pn_app_version;
+$pn_app_version = file_get_contents(dirname(__FILE__)."/version");
 global $in_cron;
 $in_cron = true;
 global $cron_tasks;
 $cron_tasks = array();
+include("install_config.inc");
 require_once("component/PNApplication.inc");
 require_once("SQLQuery.inc");
 PNApplication::$instance = new PNApplication();
--- www/cron_maintenance.php
+++ www/cron_maintenance.php
@@ -1,9 +1,12 @@
-<?php 
+<?php
+set_include_path(dirname(__FILE__));
+date_default_timezone_set("GMT");
+global $pn_app_version;
+$pn_app_version = file_get_contents(dirname(__FILE__)."/version");
 global $in_cron_maintenance;
 $in_cron_maintenance = true;
 global $cron_maintenance_tasks;
 $cron_maintenance_tasks = array();
-
 $f = fopen("maintenance_in_progress","w");
 fclose($f);
 $f = fopen("maintenance/password","w");
@@ -13,14 +16,12 @@ fwrite($f, "Automatic Maintenance");
 fclose($f);
 @unlink("maintenance_time");
 @unlink("maintenance/ask_cancel");
-
 include("install_config.inc");
 require_once("component/PNApplication.inc");
 require_once("SQLQuery.inc");
 PNApplication::$instance = new PNApplication();
 PNApplication::$instance->init();
 PNApplication::$instance->cron->executeMaintenanceTasks();
-
 @unlink("maintenance/password");
 @unlink("maintenance/origin");
 @unlink("maintenance/ask_cancel");
