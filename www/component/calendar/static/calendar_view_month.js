7af973b8af0572aa08630d7d4db7b8d652a2307b
Guillaume LE COUSIN <guillaume@lecousin.net>
2014-11-15 15:21:43 +0100

    calendar: month view

--- www/component/calendar/static/calendar_view_month.js
+++ www/component/calendar/static/calendar_view_month.js
@@ -36,22 +36,77 @@ function calendar_view_month(view, container) {
 		return null;
 	};
 	
+	// TODO back and forward
+	
 	/** Called by the CalendarView when a new event should be displayed.
 	 * @param {Object} ev the event to display
 	 */
 	this.addEvent = function(ev) {
-		var td = this._getTD(ev.start);
-		if (!td) return;
-		var div = this._createEventDiv(ev);
-		td.childNodes[0].childNodes[1].appendChild(div);
-		// TODO events on several days
+		if (ev.all_day && ev.end.getTime()-ev.start.getTime()>24*60*60*1000) {
+			// event on several days
+			// calculate first and last days, of event, and of the view
+			var first_day = Math.floor(ev.start.getTime()/(24*60*60*1000));
+			var start = new Date(); start.setUTCFullYear(this.start_date.getFullYear()); start.setUTCMonth(this.start_date.getMonth()); start.setUTCDate(this.start_date.getDate()); start.setUTCHours(0,0,0,0);
+			var first_day_of_view = Math.floor(start.getTime()/(24*60*60*1000));
+			var first_day_visible = first_day > first_day_of_view ? first_day : first_day_of_view;
+			var last_day = Math.floor(ev.end.getTime()/(24*60*60*1000));
+			var end = new Date(); end.setUTCFullYear(this.end_date.getFullYear()); end.setUTCMonth(this.end_date.getMonth()); end.setUTCDate(this.end_date.getDate()); end.setUTCHours(23,59,59,999);
+			var last_day_of_view = Math.floor(end.getTime()/(24*60*60*1000));
+			var last_day_visible = last_day < last_day_of_view ? last_day : last_day_of_view;
+			// calculate starting cell, and ending cell
+			var start_day = first_day_visible-first_day_of_view;
+			var start_week = Math.floor(start_day/7);
+			start_day -= start_week*7;
+			var end_day = last_day_visible-first_day_of_view;
+			var end_week = Math.floor(end_day/7);
+			end_day -= end_week*7;
+			for (var week = start_week; week <= end_week; ++week) {
+				// create a div
+				var div = this._createEventDiv(ev);
+				// calculate where it should be displayed
+				var start_col = week > start_week ? 0 : start_day;
+				var end_col = week < end_week ? 6 : end_day;
+				div._start_col = start_col;
+				div._end_col = end_col;
+				// insert the div at the first day
+				var content = this._tbody.childNodes[week].childNodes[start_col].childNodes[0].childNodes[1];
+				content.insertBefore(div, content.firstChild);
+				layout.changed(content);
+				// indicates overriding space to other cells
+				for (var i = start_col+1; i <= end_col; ++i) {
+					var td = this._tbody.childNodes[week].childNodes[i];
+					td._overriding_divs.push(div);
+				}
+				
+				// TODO add arrows if needed
+			}
+		} else {
+			var td = this._getTD(ev.start);
+			if (!td) return;
+			var div = this._createEventDiv(ev);
+			var content = td.childNodes[0].childNodes[1];
+			if (ev.all_day) {
+				// insert after last all_day event
+				var next = content.firstChild;
+				while (next != null && next.event.all_day) next = next.nextSibling;
+				if (!next) content.appendChild(div);
+				else content.insertBefore(div, next);
+			} else {
+				// insert after all_day events, and chronologically by time
+				var next = content.firstChild;
+				while (next != null && (next.event.all_day || next.event.start.getTime() <= ev.start.getTime())) next = next.nextSibling;
+				if (!next) content.appendChild(div);
+				else content.insertBefore(div, next);
+			}
+			layout.changed(td);
+		}
 	};
 	
 	/** Called by the CalendarView when an event needs to be removed from the dislpay.
 	 * @param {String} uid the uid of the event to remove
 	 */
 	this.removeEvent = function(uid) {
-		
+		// TODO
 	};
 	
 	this._createEventDiv = function(ev) {
@@ -99,9 +154,45 @@ function calendar_view_month(view, container) {
 		return this._tbody.childNodes[week].childNodes[day];
 	};
 	
+	t._refreshCellsLayout = function() {
+		for (var week = 0; week < this._tbody.childNodes.length; ++week) {
+			var tr = this._tbody.childNodes[week];
+			for (var day = 0; day < tr.childNodes.length; ++day) {
+				var td = tr.childNodes[day];
+				var space = 0;
+				for (var i = 0; i < td._overriding_divs.length; ++i) {
+					var div = td._overriding_divs[i];
+					var h = div.offsetTop+div.offsetHeight-div.parentNode.offsetTop;
+					if (space < h) space = h;
+				}
+				var content = td.childNodes[0].childNodes[1];
+				content.style.paddingTop = (space+1)+"px";
+				for (var i = 0; i < content.childNodes.length; ++i) {
+					var div = content.childNodes[i];
+					if (typeof div._start_col != 'undefined') {
+						// re-calculate width
+						var width = 0;
+						for (var col = div._start_col; col <= div._end_col; ++col) {
+							width += tr.childNodes[col].childNodes[0].offsetWidth+1;
+							//if (col == 0 || col == 6) width -= 1;
+						}
+						div.style.width = (width-7)+"px";
+					}
+				}
+			}
+		}
+	};
+	
 	this._layout = function() {
 		var total_width = container.clientWidth;
-		var day_width = Math.floor(total_width/7);
+		var day_width = total_width/7;
+		var widths = [];
+		var w = total_width;
+		var nb = 7;
+		for (var i = 0; i < 7; i++) {
+			widths[i] = Math.floor(w/nb--);
+			w -= widths[i];
+		}
 		// update day name in THEAD
 		for (var i = 0; i < 7; ++i)
 			this._thead.childNodes[0].childNodes[i].childNodes[0].nodeValue =
@@ -115,9 +206,10 @@ function calendar_view_month(view, container) {
 				var div = tr.childNodes[day].childNodes[0];
 				var header = div.childNodes[0];
 				var content = div.childNodes[1];
-				div.style.width = (day_width-2)+'px';
+				div.style.width = (widths[day]-1-(day==0||day==6?1:0))+'px';
 			}
 		}
+		t._refreshCellsLayout();
 	};
 	
 	this._init = function() {
@@ -149,19 +241,23 @@ function calendar_view_month(view, container) {
 				var today = new Date(); today.setHours(0,0,0,0);
 				var is_today = td.date.getTime() == today.getTime();
 				td.style.padding = "0px";
-				td.style.border = "1px solid black";
+				td.style.borderLeft = "1px solid black";
+				td.style.borderRight = "1px solid black";
+				td.style.borderTop = "2px solid black";
+				td.style.borderBottom = "2px solid black";
 				td.style.verticalAlign = "top";
-				td.style.backgroundColor = same_month ? (is_today ? "#C0FFC0" : "#FFFFFF") : "#E0E0E0";
-				if (!same_month) setOpacity(td, 0.8);
+				td._overriding_divs = [];
+				//td.style.backgroundColor = same_month ? (is_today ? "#C0FFC0" : "#FFFFFF") : "#E0E0E0";
+				//if (!same_month) setOpacity(td, 0.8);
 				tr.appendChild(td);
 				var div = document.createElement("DIV");
 				td.appendChild(div);
 				var header = document.createElement("DIV");
-				header.style.borderBottom = "1px solid black";
-				header.style.backgroundColor = is_today ? "#D0FFD0" : same_month ? "#D0D0D0" : "#E8E8E8";
+				header.style.borderBottom = "2px solid black";
+				header.style.backgroundColor = is_today ? "#D0FFD0" : same_month ? "#000000" : "#606060";
 				header.style.textAlign = "center";
-				header.style.fontWeight = is_today ? "bold" : "normal";
-				header.style.color = is_today ? "#008000" : "black";
+				header.style.fontWeight = "bold";
+				header.style.color = is_today ? "#008000" : same_month ? "white" : "#C0C0C0";
 				header.appendChild(document.createTextNode(td.date.getDate()));
 				var content = document.createElement("DIV");
 				content.style.minHeight = "10px";
@@ -180,4 +276,5 @@ function calendar_view_month(view, container) {
 	};
 	this._init();
 	layout.listenElementSizeChanged(container, function() { t._layout(); });
+	layout.listenInnerElementsChanged(container, function() { t._refreshCellsLayout(); });
 }
