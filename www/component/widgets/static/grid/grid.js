e5b48966258460c64a78f4f7358386910d765110
Guillaume LE COUSIN <guillaume@lecousin.net>
2014-12-01 10:33:33 +0100

    fix grid column container insertion at the end when grid is selectable (cherry picked from commit 384d3bd258884bf09effb72ee2e8c909b1e3bc57)

--- www/component/widgets/static/grid/grid.js
+++ www/component/widgets/static/grid/grid.js
@@ -472,14 +472,19 @@ function grid(element) {
 							if (k!=0||l!=0) matrix[i+k].push(null);
 				}
 			}
-			var tr_index = 0;
-			var i;
-			for (i = index; i > 0; i--)
-				if (matrix[level][i] != null) tr_index++;
-			if (tr_index < t.header_rows[level].childNodes.length)
-				t.header_rows[level].insertBefore(container.th, t.header_rows[level].childNodes[tr_index]);
-			else
+			var real_index = index + (t.selectable ? 1 : 0);
+			if (real_index >= matrix[level].length)
 				t.header_rows[level].appendChild(container.th);
+			else {
+				var tr_index = 0;
+				var i;
+				for (i = index; i > 0; i--)
+					if (matrix[level][i] != null) tr_index++;
+				if (tr_index < t.header_rows[level].childNodes.length)
+					t.header_rows[level].insertBefore(container.th, t.header_rows[level].childNodes[tr_index]);
+				else
+					t.header_rows[level].appendChild(container.th);
+			}
 		} else
 			t.header_rows[level].appendChild(container.th);
 		// continue insertion
