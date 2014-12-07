8606367c63e33424a117f52d8f0992ef57ae2613
Guillaume LE COUSIN <guillaume@lecousin.net>
2014-12-07 11:02:00 +0100

    additional checks when exclude an applicant, and automatically unassign from exam center or interview center if not yet scheduled to a session, else show a warning

--- www/component/selection/page/applicant/exclude.php
+++ www/component/selection/page/applicant/exclude.php
@@ -15,11 +15,25 @@ class page_applicant_exclude extends SelectionPage {
 		$q->field("Applicant","automatic_exclusion_step");
 		$q->field("Applicant","automatic_exclusion_reason");
 		$q->field("Applicant","custom_exclusion");
+		$q->field("Applicant","exam_session");
+		$q->field("Applicant","exam_center");
+		$q->field("Applicant","interview_session");
+		$q->field("Applicant","interview_center");
 		$applicants = $q->execute();
 		
 		// check if some of them are already excluded
 		$already = array();
-		foreach ($applicants as $a) if ($a["excluded"] == 1) array_push($already, $a);
+		$exam_sessions = array();
+		$interview_sessions = array();
+		$exam_center_without_session = array();
+		$interview_center_without_session = array();
+		foreach ($applicants as $a) {
+			if ($a["excluded"] == 1) array_push($already, $a);
+			if ($a["exam_session"] <> null && !in_array($a["exam_session"], $exam_sessions)) array_push($exam_sessions, $a["exam_session"]);
+			if ($a["interview_session"] <> null && !in_array($a["interview_session"], $interview_sessions)) array_push($interview_sessions, $a["interview_session"]);
+			if ($a["exam_session"] == null && $a["exam_center"] <> null) array_push($exam_center_without_session, $a);
+			if ($a["interview_session"] == null && $a["interview_center"] <> null) array_push($interview_center_without_session, $a);
+		}
 		if (count($already) > 0) {
 			echo "<div style='padding:5px'><div class='error_box'><table><tr><td valign=top><img src='".theme::$icons_16["error"]."'/></td><td>";
 			if (count($applicants) == 1)
@@ -39,6 +53,56 @@ class page_applicant_exclude extends SelectionPage {
 			echo "</td></tr></table></div></div>";
 			return;
 		}
+		if (count($exam_sessions) > 0) {
+			// check if in the future
+			require_once("component/calendar/CalendarJSON.inc");
+			$events = CalendarJSON::getEventsFromDB($exam_sessions, PNApplication::$instance->selection->getCalendarId());
+			$exam_sessions = array();
+			foreach ($events as $ev) if ($ev["start"] > time()) array_push($exam_sessions, $ev["id"]);
+			if (count($exam_sessions) > 0) {
+				echo "<div style='padding:5px'><div class='warning_box'><table><tr><td valign=top><img src='".theme::$icons_16["warning"]."'/></td><td>";
+				echo "The following applicants are assign to a future exam session, and you will probably need to remove them from the exam sessions if you exclude them:<ul>";
+				foreach ($applicants as $a) {
+					if (in_array($a["exam_session"], $exam_sessions))
+						echo "<li>".toHTML($a["first_name"]." ".$a["last_name"]).",  ID ".$a["applicant_id"]."</li>";
+				}
+				echo "</ul>";
+				echo "</td></tr></table></div></div>";
+			}
+		}
+		if (count($interview_sessions) > 0) {
+			// check if in the future
+			require_once("component/calendar/CalendarJSON.inc");
+			$events = CalendarJSON::getEventsFromDB($interview_sessions, PNApplication::$instance->selection->getCalendarId());
+			$interview_sessions = array();
+			foreach ($events as $ev) if ($ev["start"] > time()) array_push($interview_sessions, $ev["id"]);
+			if (count($interview_sessions) > 0) {
+				echo "<div style='padding:5px'><div class='warning_box'><table><tr><td valign=top><img src='".theme::$icons_16["warning"]."'/></td><td>";
+				echo "The following applicants are assign to a future interview session, and you will probably need to remove them from the interview sessions if you exclude them:<ul>";
+				foreach ($applicants as $a) {
+					if (in_array($a["interview_session"], $interview_sessions))
+						echo "<li>".toHTML($a["first_name"]." ".$a["last_name"]).",  ID ".$a["applicant_id"]."</li>";
+				}
+				echo "</ul>";
+				echo "</td></tr></table></div></div>";
+			}
+		}
+		if (count($exam_center_without_session) > 0) {
+			echo "<div style='padding:5px'><div class='info_box'><table><tr><td valign=top><img src='".theme::$icons_16["info"]."'/></td><td>";
+			echo "The following applicants are assign to an exam center, but not yet scheduled to an exam session. Please note they will be automatically unassigned from their exam center:<ul>";
+			foreach ($exam_center_without_session as $a)
+				echo "<li>".toHTML($a["first_name"]." ".$a["last_name"]).",  ID ".$a["applicant_id"]."</li>";
+			echo "</ul>";
+			echo "</td></tr></table></div></div>";
+		}
+		if (count($interview_center_without_session) > 0) {
+			echo "<div style='padding:5px'><div class='info_box'><table><tr><td valign=top><img src='".theme::$icons_16["info"]."'/></td><td>";
+			echo "The following applicants are assign to an interview center, but not yet scheduled to an interview session. Please note they will be automatically unassigned from their interview center:<ul>";
+			foreach ($interview_center_without_session as $a)
+				echo "<li>".toHTML($a["first_name"]." ".$a["last_name"]).",  ID ".$a["applicant_id"]."</li>";
+			echo "</ul>";
+			echo "</td></tr></table></div></div>";
+		}
 		
 ?>
 <div style='background-color:white;padding:10px'>
@@ -53,7 +117,46 @@ function exclude() {
 	var popup = window.parent.get_popup_window_from_frame(window);
 	popup.freeze("Excluding applicant<?php if (count($applicants) > 1) echo "s"?>...");
 	var ids = <?php echo json_encode($applicants_ids);?>;
-	service.json("data_model","save_cells",{cells:[{table:'Applicant',sub_model:<?php echo $this->component->getCampaignId();?>,keys:ids,values:[{column:'excluded',value:1},{column:'custom_exclusion',value:reason}]}]},function(res) {
+	service.json("data_model","save_cells",{
+			cells:[
+				{
+					table:'Applicant',
+					sub_model:<?php echo $this->component->getCampaignId();?>,
+					keys:ids,
+					values:[{column:'excluded',value:1},{column:'custom_exclusion',value:reason}]
+				}
+				<?php 
+				if (count($exam_center_without_session) > 0) {
+					echo ",{";
+					echo "table:'Applicant',";
+					echo "sub_model:".$this->component->getCampaignId().",";
+					echo "keys:[";
+					$first = true;
+					foreach ($exam_center_without_session as $a) {
+						if ($first) $first = false; else echo ",";
+						echo $a["people_id"];
+					}
+					echo "],";
+					echo "values:[{column:'exam_center',value:null}]";
+					echo "}";
+				}
+				if (count($interview_center_without_session) > 0) {
+					echo ",{";
+					echo "table:'Applicant',";
+					echo "sub_model:".$this->component->getCampaignId().",";
+					echo "keys:[";
+					$first = true;
+					foreach ($interview_center_without_session as $a) {
+						if ($first) $first = false; else echo ",";
+						echo $a["people_id"];
+					}
+					echo "],";
+					echo "values:[{column:'interview_center',value:null}]";
+					echo "}";
+				}
+				?>
+			]
+	},function(res) {
 		<?php if (isset($_GET["ondone"])) echo "window.frameElement.".$_GET["ondone"]."();"?>
 		popup.close();
 	});
