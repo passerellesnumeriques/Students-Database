444f8d7fe926f3dc4853910fbe35052c1b34d2ed
Guillaume LE COUSIN <guillaume@lecousin.net>
2014-12-11 13:46:41 +0100

    fix deploy_utils.js when resuming download

--- www/component/application/static/deploy_utils.js
+++ www/component/application/static/deploy_utils.js
@@ -161,9 +161,12 @@ function downloading(backend_url, download_url, size, file, mirrors_provider_inf
 			var stop = progress_handler(end+1,size);
 			if (end >= size-1) { end_handler(null); return; }
 			if (content.substring(0,6) == "cache:") {
-				end = parseInt(content.substring(6));
-				stop = progress_handler(end+1,size);
-				if (end >= size-1) { end_handler(null); return; }
+				var size_already_downloaded = parseInt(content.substring(6));
+				if (!isNaN(size_already_downloaded)) {
+					end = size_already_downloaded-1;
+					stop = progress_handler(end+1,size);
+					if (end >= size-1) { end_handler(null); return; }
+				}
 			} else {
 				var end_time = new Date().getTime();
 				if (end_time-start_time < 2000) speed *= 10;
