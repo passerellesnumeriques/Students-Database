7601f4d820df67c4c7abfd88e98f27e2419061ec
Guillaume LE COUSIN <guillaume@lecousin.net>
2014-12-31 12:37:07 +0100

    field_contact_type: can fill all rows with same type

--- www/component/contact/static/field_contact_type.js
+++ www/component/contact/static/field_contact_type.js
@@ -7,6 +7,67 @@
  */
 function field_contact_type(data,editable,config) {
 	typed_field_multiple.call(this, data, editable, config);
+	if (typeof config.sub_data_index != 'undefined' && config.sub_data_index == 0) {
+		this.helpFillMultipleItems = function() {
+			var helper = {
+				title: "Fill contact type",
+				content: document.createElement("DIV"),
+				apply: function(field) {
+					var type = this.input.value.trim();
+					var choice;
+					for (var i = 0; i < this.radios.length; ++i)
+						if (this.radios[i].checked) { choice = this.radios[i].value; break; }
+					if (choice == "new") {
+						field.addData(type);
+					} else {
+						var index;
+						if (choice == "last") {
+							index = field.getNbData()-1;
+						} else {
+							index = parseInt(choice)-1;
+						}
+						if (index >= 0 && index < field.getNbData())
+							field.setDataIndex(index, type);
+					}
+				}
+			};
+			var div = document.createElement("DIV");
+			helper.content.appendChild(div);
+			div.appendChild(document.createTextNode("Contact Type: "));
+			helper.input = document.createElement("INPUT");
+			helper.input.size = 15;
+			helper.input.maxLength = 30;
+			div.appendChild(helper.input);
+			helper.input.onclick = function() {
+				require("contact_objects.js", function() {
+					showContactTypeMenu(helper.input,config.type,helper.input.value,false,function(new_type) {
+						helper.input.value = new_type;
+					});
+				});
+			};
+			helper.radios = [];
+			var addRadio = function(text, value, selected) {
+				var div = document.createElement("DIV");
+				var radio = document.createElement("INPUT");
+				radio.type = "radio";
+				radio.value = value;
+				radio.name = "contact_type_choice";
+				helper.radios.push(radio);
+				div.appendChild(radio);
+				div.appendChild(document.createTextNode(" "+text));
+				if (selected) radio.checked = 'checked';
+				helper.content.appendChild(div);
+			};
+			addRadio("Create a new contact with this type", "new", true);
+			addRadio("Set the first contact with this type (if one exists)", "1");
+			addRadio("Set the second contact with this type (if at least two exist)", "2");
+			addRadio("Set the third contact with this type (if at least three exist)", "3");
+			addRadio("Set the fourth contact with this type (if at least four exist)", "4");
+			addRadio("Set the fifth contact with this type (if at least five exist)", "5");
+			addRadio("Set the last contact with this type (if at least one exists)", "last");
+			return helper;
+		};
+	}
 }
 field_contact_type.prototype = new typed_field_multiple();
 field_contact_type.prototype.constructor = field_contact_type;	
@@ -76,11 +137,11 @@ field_contact_type.prototype._create = function(data) {
 		this.element.style.height = "100%";
 		if (this.editable) {
 			this.getNbData = function() {
-				return t._data.contacts.length;
+				return this._data.contacts.length;
 			};
 			this.resetData = function() {
-				t._data.contacts = [];
-				t.setData(t._data, true);
+				this._data.contacts = [];
+				t.setData(this._data, true);
 			};
 
 			if (this.config.sub_data_index == 0) {
@@ -117,6 +178,7 @@ field_contact_type.prototype._create = function(data) {
 				this._setData(data);
 				
 				this.addData = function(new_data) {
+					var t=this;
 					require("contact_objects.js", function() {
 						var contact = new Contact(-1, t.config.type, new_data, "");
 						t._data.contacts.push(contact);
@@ -124,11 +186,11 @@ field_contact_type.prototype._create = function(data) {
 					});
 				};
 				this.getDataIndex = function(index) {
-					return t._data.contacts[index].sub_type;
+					return this._data.contacts[index].sub_type;
 				};
 				this.setDataIndex = function(index, new_data) {
-					t._data.contacts[index].sub_type = new_data;
-					t.setData(t._data, true);
+					this._data.contacts[index].sub_type = new_data;
+					this.setData(this._data, true);
 				};
 			} else {
 				// contact
@@ -171,6 +233,7 @@ field_contact_type.prototype._create = function(data) {
 				this._setData(data);
 				
 				this.addData = function(new_data) {
+					var t=this;
 					require("contact_objects.js", function() {
 						var contact = new Contact(-1, t.config.type, "", new_data);
 						t._data.contacts.push(contact);
@@ -178,11 +241,11 @@ field_contact_type.prototype._create = function(data) {
 					});
 				};
 				this.getDataIndex = function(index) {
-					return t._data.contacts[index].contact;
+					return this._data.contacts[index].contact;
 				};
 				this.setDataIndex = function(index, new_data) {
-					t._data.contacts[index].contact = new_data;
-					t.setData(t._data, true);
+					this._data.contacts[index].contact = new_data;
+					this.setData(this._data, true);
 				};
 			}
 		} else {
