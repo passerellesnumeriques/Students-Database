2c19f8831247fe030e671fd22f0a93bd7e170003
Guillaume LE COUSIN <guillaume@lecousin.net>
2015-01-05 13:33:34 +0100

    finance: can cancel all following operations in a serie

--- www/component/finance/page/operation.php
+++ www/component/finance/page/operation.php
@@ -23,11 +23,12 @@ class page_operation extends Page {
 		$schedule = SQLQuery::create()->select("ScheduledPaymentDate")->whereValue("ScheduledPaymentDate","due_operation",$op["id"])->executeSingleRow();
 		$payments = SQLQuery::create()->select("PaymentOperation")->whereValue("PaymentOperation","due_operation",$op["id"])->join("PaymentOperation","FinanceOperation",array("payment_operation"=>"id"))->orderBy("FinanceOperation","date")->execute();
 		$descr = "";
+		$due_date_ts = datamodel\ColumnDate::toTimestamp($op["date"]);
+		$due_date = date("d M Y", $due_date_ts);
 		if ($schedule <> null) {
 			if ($schedule["regular_payment"] <> null) {
 				// this operation comes from a general regular payment
 				$regular_payment = SQLQuery::create()->select("FinanceRegularPayment")->whereValue("FinanceRegularPayment","id",$schedule["regular_payment"])->executeSingleRow();
-				$due_date_ts = datamodel\ColumnDate::toTimestamp($op["date"]);
 				switch ($regular_payment["frequency"]) {
 					case "Daily":
 					case "Weekly":
@@ -42,10 +43,26 @@ class page_operation extends Page {
 				}
 				$descr = $regular_payment["name"];
 				$this->setPopupTitle($regular_payment["name"]." of ".$date_str." for ".$people["first_name"]." ".$people["last_name"]);
+				$due_date = $date_str;
+				// check if we have next payments also which can be cancelled
+				$next_operations = SQLQuery::create()
+					->select("FinanceOperation")
+					->whereValue("FinanceOperation","people",$op["people"])
+					->where("`FinanceOperation`.`date` > '".$op["date"]."'")
+					->join("FinanceOperation", "ScheduledPaymentDate", array("id"=>"due_operation"))
+					->whereValue("ScheduledPaymentDate", "regular_payment", $schedule["regular_payment"])
+					->join("FinanceOperation","PaymentOperation",array("id"=>"due_operation"))
+					->execute();
+				if (count($next_operations) > 0) {
+					$has_next_payment = false;
+					foreach ($next_operations as $o) if ($o["payment_operation"] <> null) { $has_next_payment = true; break; }
+					if (!$has_next_payment)
+						$can_cancel_next_operations = true;
+				}
 			}
 		}
 		echo "<table>";
-		echo "<tr><td>Due Date:</td><td align=right>".$op["date"]."</td></tr>";
+		echo "<tr><td>Due Date:</td><td align=right>$due_date</td></tr>";
 		echo "<tr><td>Amount Due:</td><td align=right>".(-floatval($op["amount"]))."</td></tr>";
 		$balance = floatval($op["amount"]);
 		if (count($payments) == 0)
@@ -71,6 +88,37 @@ class page_operation extends Page {
 			echo "<button class='action red' onclick=\"location.href='cancel_due_operation?id=".$op["id"].(isset($_GET["onchange"]) ? "&ondone=".$_GET["onchange"] : "")."';\">Cancel this ".toHTML($descr)."</button>";
 		if ($balance < 0 && count($payments) > 0)
 			echo "<button class='action red' onclick=\"confirmDialog('Do you confirm the remaining balance is cancelled for this student ?',function(yes){if(!yes)return; service.json('finance','save_operation',{id:".$op["id"].",amount:".(floatval($op["amount"])-$balance)."},function(res){if(!res)return;".(isset($_GET["onchange"]) ? "window.frameElement.".$_GET["onchange"]."();" : "")."location.reload();});});\">Cancel remaining balance of ".(-$balance)."</button>";
+		if (@$can_cancel_next_operations && count($payments) == 0) {
+			echo "<br/><button class='action red' onclick='cancelNextOperations();'>Cancel all ".toHTML($descr)." starting from $due_date</button>";
+			?>
+			<script type='text/javascript'>
+			function cancelNextOperations() {
+				confirmDialog("Are you sure you want to cancel this "+<?php echo json_encode($descr);?>+" and all the next ones ?",function(yes) {
+					if (!yes) return;
+					var popup = window.parent.getPopupFromFrame(window);
+					popup.freeze("Cancelling operations...");
+					<?php
+					$nb = 0; 
+					echo "service.json('finance','remove_operation',{id:".$op["id"]."},function(res) {";
+					echo "if (!res) { popup.unfreeze(); return; }";
+					$nb++;
+					foreach ($next_operations as $o) {
+						echo "service.json('finance','remove_operation',{id:".$o["id"]."},function(res) {";
+						echo "if (!res) { popup.unfreeze(); return; }";
+						$nb++;
+					}
+					if (isset($_GET["onchange"])) echo "window.frameElement.".$_GET["onchange"]."();";
+					echo "popup.close();";
+					while ($nb > 0) {
+						echo "});";
+						$nb--;
+					}
+					?>
+				});
+			}
+			</script>
+			<?php 
+		}
 	}
 	
 	private function paymentOperation($op, $people) {
