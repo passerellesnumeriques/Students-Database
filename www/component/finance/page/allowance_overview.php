eaeb0507c52a1ee49176f5325792cd8d9e6e4c45
Guillaume LE COUSIN <guillaume@lecousin.net>
2015-01-17 15:45:03 +0100

    set allowance base amount

---
+++ www/component/finance/page/allowance_overview.php
@@ -0,0 +1,175 @@
+<?php 
+class page_allowance_overview extends Page {
+	
+	public function getRequiredRights() { return array("consult_student_finance"); }
+	
+	public function execute() {
+		require_once 'component/students_groups/page/TreeFrameSelection.inc';
+		$allowance_id = $_GET["id"];
+		$allowance = SQLQuery::create()->select("Allowance")->whereValue("Allowance","id",$allowance_id)->executeSingleRow();
+		$can_manage = PNApplication::$instance->user_management->hasRight("manage_finance");
+		$can_edit = PNApplication::$instance->user_management->hasRight("edit_student_finance");
+		
+		if ($can_edit) {
+			$this->requireJavascript("typed_field.js");
+			$this->requireJavascript("field_decimal.js");
+			$this->requireJavascript("popup_window.js");
+		}
+		
+		theme::css($this, "grid.css");
+?>
+<div style='width:100%;height:100%;display:flex;flex-direction:column;'>
+	<div class='page_title' style='flex:none'>
+		<?php
+		if (TreeFrameSelection::isSingleBatch() && $can_manage)
+			echo "<button class='action red' style='float:right' onclick='removeForBatch(".TreeFrameSelection::getBatchId().");'><img src='".theme::$icons_16["remove_white"]."'/> Remove</button>"; 
+		?>
+		<img src='/static/finance/finance_32.png'/>
+		<?php echo toHTML($allowance["name"])?>
+		<div style='display:inline-block;font-size:12pt;margin-left:10px;font-style:italic'>
+			<?php
+			$batch_id = TreeFrameSelection::getBatchId();
+			if ($batch_id <> null)
+				echo "Batch ".toHTML(TreeFrameSelection::getBatchName());
+			else if (TreeFrameSelection::isAllBatches()) echo "All batches";
+			else if (TreeFrameSelection::isCurrentBatches()) echo "Current batches";
+			else if (TreeFrameSelection::isAlumniBatches()) echo "Alumni batches";
+			?>
+		</div>
+	</div>
+	<div style='flex: 1 1 100%;overflow:auto;background-color:white'>
+		<?php 
+		$batches_ids = TreeFrameSelection::getBatchesIds();
+		$batches = PNApplication::$instance->curriculum->getBatches($batches_ids);
+		foreach ($batches as $batch) {
+			$group_id = TreeFrameSelection::getGroupId();
+			$period_id = TreeFrameSelection::getPeriodId();
+			if ($group_id <> null) {
+				$q = PNApplication::$instance->students_groups->getStudentsQueryForGroup($group_id);
+				PNApplication::$instance->people->joinPeople($q, "StudentGroup", "people", false); 
+			} else if ($period_id <> null) {
+				$spe_id = TreeFrameSelection::getSpecializationId();
+				if ($spe_id == null) $spe_id = false;
+				$students_ids = PNApplication::$instance->students_groups->getStudentsForPeriod($period_id, $spe_id);
+				$q = PNApplication::$instance->people->getPeoplesSQLQuery($students_ids,false,true);
+			} else {
+				$q = PNApplication::$instance->students->getStudentsQueryForBatches(array($batch["id"]));
+				PNApplication::$instance->people->joinPeople($q, "Student", "people", false);
+			}
+			$q->orderBy("People","last_name");
+			$q->orderBy("People","first_name");
+			$list = $q->execute();
+			if (!TreeFrameSelection::isSingleBatch()) {
+				echo "<div class='page_section_title'>";
+				echo "Batch ".toHTML($batch["name"]);
+				if (count($schedules) > 0 && $can_manage)
+					echo "<button class='action red' style='float:right' onclick='removeForBatch(".$batch["id"].");'><img src='".theme::$icons_16["remove_white"]."'/> Remove</button>";
+				echo "</div>";
+				echo "<div style='border-bottom:1px solid black;margin-bottom:5px;'>";
+			}
+			if (count($list) == 0) {
+				echo "<i>No student in this batch</i>";
+				if (!TreeFrameSelection::isSingleBatch()) echo "</div>";
+				continue;
+			}
+			if ($can_edit) {
+				echo "<div class='page_section_title shadow'>";
+				echo "<button class='action' onclick='setBaseAmountForBatch(".$batch["id"].");'>Set Base Amount</button>";
+				echo "</div>";
+			}
+			$students = array();
+			foreach ($list as $student) $students[$student["people_id"]] = $student;
+			$base_amount = SQLQuery::create()
+				->select("StudentAllowance")
+				->whereValue("StudentAllowance","allowance",$allowance_id)
+				->whereIn("StudentAllowance","student",array_keys($students))
+				->whereNull("StudentAllowance","date")
+				->execute()
+				;
+			foreach ($base_amount as $a) $students[$a["student"]]["base_amount"] = $a;
+			echo "<table class='grid selected_hover'><thead>";
+			echo "<tr>";
+				echo "<th>Student</th>";
+				echo "<th>Base amount</th>";
+			echo "</tr>";
+			echo "</thead><tbody>";
+			foreach ($students as $student) {
+				echo "<tr student_name=".toHTMLAttribute($student["last_name"]." ".$student["first_name"]).">";
+				echo "<td>".toHTML($student["last_name"]." ".$student["first_name"])."</td>";
+				if (!isset($student["base_amount"])) {
+					echo "<td style='font-style:italic;cursor:pointer;' title='Click to set an allowance for this student' onclick='setBaseAmountForStudent(".$student["people_id"].",undefined,this.parentNode);'>No allowance</td>";
+				} else {
+					echo "<td style='text-align:right;cursor:pointer' title='Click to edit the base amount for this student' onclick='setBaseAmountForStudent(".$student["people_id"].",".$student["base_amount"]["amount"].",this.parentNode);'>".$student["base_amount"]["amount"]."</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</tbody></table>";
+			if (!TreeFrameSelection::isSingleBatch()) echo "</div>";
+		}
+		?>
+	</div>
+</div>
+<div style='display:none;padding:10px;' id='popup_set_base_amount_for_batch'>
+	Enter the base amount for all students: <span id='base_amount_for_batch'></span>
+</div>
+<div style='display:none;padding:10px;' id='popup_set_base_amount_for_student'>
+	Enter the base amount: <span id='base_amount_for_student'></span>
+</div>
+<script type='text/javascript'>
+var batches = <?php echo json_encode($batches);?>;
+
+function removeForBatch(batch_id) {
+	var batch = null;
+	for (var i = 0; i < batches.length; ++i) if (batches[i].id == batch_id) { batch = batches[i]; break; }
+	confirmDialog("Are you sure you want to remove <?php echo $allowance["name"];?> for Batch "+batch.name+" ?",function(yes) {
+		if (!yes) return;
+		// TODO
+	});
+}
+
+<?php if ($can_edit) { ?>
+var base_amount_for_batch = new field_decimal(1,true,{can_be_null:false,min:1,integer_digits:10,decimal_digits:2});
+document.getElementById('base_amount_for_batch').appendChild(base_amount_for_batch.getHTMLElement());
+function setBaseAmountForBatch(batch_id) {
+	var popup = new popup_window("Set Base Amount For Batch",null,document.getElementById('popup_set_base_amount_for_batch'));
+	popup.keep_content_on_close = true;
+	base_amount_for_batch.setData(1);
+	popup.addOkCancelButtons(function() {
+		if (base_amount_for_batch.hasError()) { alert("Please enter a valid amount"); return; }
+		popup.freeze("Setting base amount...");
+		service.json("finance","set_allowance_base_amount",{batch:batch_id,amount:base_amount_for_batch.getCurrentData(),allowance:<?php echo $allowance_id;?>},function(res) {
+			if (!res)
+				popup.unfreeze();
+			else
+				location.reload();
+		});
+	});
+	popup.show();
+}
+
+var base_amount_for_student = new field_decimal(1,true,{can_be_null:false,min:1,integer_digits:10,decimal_digits:2});
+document.getElementById('base_amount_for_student').appendChild(base_amount_for_student.getHTMLElement());
+function setBaseAmountForStudent(student_id, current_amount, tr) {
+	var student_name = tr.getAttribute("student_name");
+	var popup = new popup_window("Set Base Amount For Student "+student_name,null,document.getElementById('popup_set_base_amount_for_student'));
+	popup.keep_content_on_close = true;
+	base_amount_for_student.setData(current_amount ? current_amount : 1);
+	popup.addOkCancelButtons(function() {
+		if (base_amount_for_student.hasError()) { alert("Please enter a valid amount"); return; }
+		popup.freeze("Setting base amount...");
+		service.json("finance","set_allowance_base_amount",{student:student_id,amount:base_amount_for_student.getCurrentData(),allowance:<?php echo $allowance_id;?>},function(res) {
+			if (!res)
+				popup.unfreeze();
+			else
+				location.reload();
+		});
+	});
+	popup.show();
+}
+<?php } ?>
+</script>
+<?php 
+	}
+	
+}
+?>
---
+++ www/component/finance/service/set_allowance_base_amount.php
@@ -0,0 +1,47 @@
+<?php 
+class service_set_allowance_base_amount extends Service {
+	
+	public function getRequiredRights() { return array("edit_student_finance"); }
+	
+	public function documentation() { echo "Set the base amount for an allowance"; }
+	public function inputDocumentation() { echo "allowance,amount,(batch|student)"; }
+	public function outputDocumentation() { echo "true on success"; }
+	
+	public function execute(&$component, $input) {
+		SQLQuery::startTransaction();
+		if (isset($input["batch"]))
+			$students = PNApplication::$instance->students->getStudentsIdsForBatch($input["batch"]);
+		else
+			$students = array($input["student"]);
+		$existing = SQLQuery::create()
+			->select("StudentAllowance")
+			->whereValue("StudentAllowance","allowance",$input["allowance"])
+			->whereIn("StudentAllowance","student",$students)
+			->whereNull("StudentAllowance","date")
+			->execute();
+		$update = array();
+		foreach ($existing as $e) {
+			array_push($update, $e["id"]);
+			array_splice($students, array_search($e["student"], $students), 1);
+		}
+		if (count($update) > 0)
+			SQLQuery::create()->updateAllKeys("StudentAllowance", $update, array("amount"=>$input["amount"]));
+		if (count($students) > 0) {
+			$insert = array();
+			foreach ($students as $student)
+				array_push($insert, array(
+					"student"=>$student,
+					"allowance"=>$input["allowance"],
+					"date"=>null,
+					"amount"=>$input["amount"]
+				));
+			SQLQuery::create()->insertMultiple("StudentAllowance", $insert);
+		}
+		if (!PNApplication::hasErrors()) {
+			SQLQuery::commitTransaction();
+			echo "true";
+		}
+	}
+	
+}
+?>
